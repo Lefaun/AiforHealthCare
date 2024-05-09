1b18f473a99ce55c66599a0dc127c568988c50e9
Lefaun <118660975+Lefaun@users.noreply.github.com>
2024-05-09 10:00:12 +0100

    Update Main.py

--- Main.py
+++ Main.py
@@ -149,7 +149,9 @@ def menu():
         
         
         
-        
+        chart_data = pd.DataFrame(Lista, columns= ["Ferrantina", "Hemoglobina","Leucócitos","Creatinina","Proteina C-reativa", "bmi", "imc",  "pressao_arterial", "LDL_colesterol" , "HDL_colesterol2",  "log_proteina_total", "log_LDL/HDL",  "média_acucar_jejum"])
+
+        st.bar_chart(chart_data)
 
         
 
